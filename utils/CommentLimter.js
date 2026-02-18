f30cc5bba291f149bea98e3d2806f987c949bf7a
sj-10ps <sps24723@gmail.com>
2026-02-18 13:42:19 +0530

    added redis for rate limit

--- utils/CommentLimter.js
+++ utils/CommentLimter.js
@@ -1,7 +1,22 @@
 const limiter=require('express-rate-limit')
+const {createClient} = require('redis');
+const {RedisStore} = require('rate-limit-redis');
+const redisClient=createClient({
+    url: 'redis://sjps:sJ@123456@redis-13725.c85.us-east-1-2.ec2.cloud.redislabs.com:13725'
+})
+redisClient.connect().catch(console.error)
+
+redisClient.on('connect',()=>{
+    console.log('Connected to redis')
+})
+
 const CommentLimiter=limiter({
+    store:new RedisStore({
+         sendCommand:(...args)=>redisClient.sendCommand(args)   
+    }),
     windowMs:15*60*1000,
     max:5,
+    standardHeaders:true,
     handler:(req,res)=>{
         res.status(429).json({message:"too many attempts please try again later"})
     }
--- utils/loginRatelimit.js
+++ utils/loginRatelimit.js
@@ -1,5 +1,17 @@
 const limiter=require('express-rate-limit')
+const {createClient} =require('redis')
+const {RedisStore}=require('rate-limit-redis')
+const redisClient=createClient({
+    url:'redis://sjps:sJ@123456@redis-13725.c85.us-east-1-2.ec2.cloud.redislabs.com:13725'
+})
+redisClient.connect().catch(console.error)
+redisClient.on('connect',()=>{
+   console.log('redis connected')
+})
 const loginLimiter=limiter({
+    store:new RedisStore({
+        sendCommand:(...args)=>redisClient.sendCommand(args)
+    }),
     windowMs:15*60*1000,
     max:5,
     handler:(req,res)=>{
